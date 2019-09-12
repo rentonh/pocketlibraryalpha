c4ab16e72754d8bc2666390c5bc022a745bb832e
Rentonh <rentonhxyc@gmail.com>
2019-09-12 19:54:26 +0930

    fixed HBG icon animation, remove HBG menu for later. fixed and repositioned left and right arrow. added switch picture function and connect with arrows. added level navigation icons. added related images.
    Signed-off-by: Rentonh <rentonhxyc@gmail.com>

--- js/js.js
+++ js/js.js
@@ -1,22 +1,49 @@
-// hamburger menu //
+// hamburger menu
 var button = document.getElementById('hamburger-menu'),
     span = button.getElementsByTagName('span')[0];
 
 button.onclick =  function() {
   span.classList.toggle('hamburger-menu-button-close');
 };
-
-$('#hamburger-menu').on('click', toggleOnClass);
+//$('#hamburger-menu').on('click', toggleOnClass);
 
 function toggleOnClass(event) {
   var toggleElementId = '#' + $(this).data('toggle'),
   element = $(toggleElementId);
-
   element.toggleClass('on');
+}
+  // close hamburger menu after click a //
+//$( '.menu li a' ).on("click", function(){
+  //$('#hamburger-menu').click();
+//});
 
+// Open HBG Menu
+function openMenu() {
+  var x = document.getElementById('HBGMenu');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
 }
 
-  // close hamburger menu after click a //
-$( '.menu li a' ).on("click", function(){
-  $('#hamburger-menu').click();
-});
+// left and right arrow slide
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mainframe-mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+// Change floor level
