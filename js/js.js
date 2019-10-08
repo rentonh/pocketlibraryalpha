5692fa3f464daa5952dbad83a4353208ab57cca5
Rentonh <rentonhxyc@gmail.com>
2019-10-09 00:47:19 +0930

    Added bookshelf interface on floor three. this is first half part of bookshelf function. this part includes frame base, slider and swipe. images 111,222,333,444 are temporary file that helps to test if it works.
    Signed-off-by: Rentonh <rentonhxyc@gmail.com>

--- js/js.js
+++ js/js.js
@@ -1,31 +1,3 @@
-// hamburger menu
-var button = document.getElementById('hamburger-menu'),
-    span = button.getElementsByTagName('span')[0];
-
-button.onclick =  function() {
-  span.classList.toggle('hamburger-menu-button-close');
-};
-//$('#hamburger-menu').on('click', toggleOnClass);
-
-function toggleOnClass(event) {
-  var toggleElementId = '#' + $(this).data('toggle'),
-  element = $(toggleElementId);
-  element.toggleClass('on');
-}
-  // close hamburger menu after click a //
-//$( '.menu li a' ).on("click", function(){
-  //$('#hamburger-menu').click();
-//});
-
-// Open HBG Menu
-function openMenu() {
-  var x = document.getElementById('HBGMenu');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-  } else {
-    x.style.display = 'none';
-  }
-}
 
 // Show Home Image
 
@@ -98,11 +70,63 @@ function hideLeftArrow() {
   hideArwL.style.display = "none";
 }
 
+// Set main image to home page
+showHome();
+
 //popup test
 function popup() {
   var pop = document.getElementById("poppop")
   pop.classList.toggle("show");
 }
 
-// Set main image to home page
-showHome();
+
+
+// hamburger menu
+var button = document.getElementById('hamburger-menu'),
+    span = button.getElementsByTagName('span')[0];
+
+button.onclick =  function() {
+  span.classList.toggle('hamburger-menu-button-close');
+};
+//$('#hamburger-menu').on('click', toggleOnClass);
+
+function toggleOnClass(event) {
+  var toggleElementId = '#' + $(this).data('toggle'),
+  element = $(toggleElementId);
+  element.toggleClass('on');
+}
+  // close hamburger menu after click a //
+//$( '.menu li a' ).on("click", function(){
+  //$('#hamburger-menu').click();
+//});
+
+// Open HBG Menu
+function openMenu() {
+  var x = document.getElementById('HBGMenu');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
+
+
+
+//carousel Bookshelf
+$(document).ready(function(){
+  var slider = $('.slider').bxSlider({
+      mode: 'horizontal', //mode: 'fade',
+      speed: 300,
+      auto: false,
+      infiniteLoop: true,
+      hideControlOnEnd: true,
+      useCSS: false,
+      LoadAfterUI: true,
+      onSliderLoad: function(currentIndex) {
+        $().filter('[data-picindex="'+(currentIndex+1)+'"]').addClass('active').siblings().removeClass('active');
+      },
+      onSlideAfter: function($slideElement, oldIndex, newIndex) {
+        $().filter('[data-picindex="'+(newIndex+1)+'"]').addClass('active').siblings().removeClass('active');
+      }
+  });
+});
