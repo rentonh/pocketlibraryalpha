78b1b129c8e0a0bb461456b36cd65147d9f33fb6
Rentonh <rentonhxyc@gmail.com>
2019-10-18 00:21:29 +0930

    Task facilities indicator update facilities to main images update home image include location and CDU icon Hide hamburger menu html and javascript (abandoned function)
    Signed-off-by: Rentonh <rentonhxyc@gmail.com>

--- js/js.js
+++ js/js.js
@@ -134,7 +134,7 @@ showHome();
 
 
 // hamburger menu
-var button = document.getElementById('hamburger-menu'),
+/*var button = document.getElementById('hamburger-menu'),
     span = button.getElementsByTagName('span')[0];
 
 button.onclick =  function() {
@@ -146,7 +146,7 @@ function toggleOnClass(event) {
   var toggleElementId = '#' + $(this).data('toggle'),
   element = $(toggleElementId);
   element.toggleClass('on');
-}
+}*/
   // close hamburger menu after click a //
 //$( '.menu li a' ).on("click", function(){
   //$('#hamburger-menu').click();
